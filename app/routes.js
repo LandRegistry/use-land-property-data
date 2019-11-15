8caed4dfd9a9e60828eecb25fc9fb3e7e68cd925
ztrelease <zoe.trelease@digital.landregistry.gov.uk>
2019-11-15 17:51:58 +0000

    sprint 1

--- app/routes.js
+++ app/routes.js
@@ -10,12 +10,97 @@ router.use('/node_modules', express.static('node_modules'))
 // /////////////////////////
 
 router.post('/form-handler', function (req, res) {
-  
+
   // serverside routing logic here
 
   // redirect to form complete page
 	res.redirect('form-submitted')
 
+})
+
+
+
+// choosing the account type to create
+router.post('/account-choice', function (req, res) {
+
+  let choice = req.session.data['account-choice']
+
+  if (choice === 'personal') {
+    res.redirect('version-0-1/create-account/personal-account/your-details')
+  } else if (choice == 'uk-org') {
+    res.redirect('version-0-1/create-account/uk-org-account/your-details')
+  } else if (choice == 'os-org') {
+    res.redirect('version-0-1/create-account/overseas-org-account/your-details')
+  }
+})
+
+
+// whether user lives in the UK or not
+router.post('/live-in-uk', function (req, res) {
+
+  let choice = req.session.data['live-in-uk']
+
+  if (choice === 'yes') {
+    res.redirect('version-0-1/create-account/personal-account/uk-address')
+  } else {
+    res.redirect('version-0-1/create-account/personal-account/overseas-address')
+  }
+})
+
+
+// whether user wants to partake in research
+router.post('/personal-research', function (req, res) {
+
+  let choice = req.session.data['personal-research']
+
+  if (choice === 'yes') {
+    res.redirect('version-0-1/create-account/personal-account/yes-research')
+  } else {
+    res.redirect('version-0-1/create-account/personal-account/check-answers')
+  }
+})
+
+
+// whether user wants to partake in research
+router.post('/uk-org-research', function (req, res) {
+
+  let choice = req.session.data['uk-org-research']
+
+  if (choice === 'yes') {
+    res.redirect('version-0-1/create-account/uk-org-account/yes-research')
+  } else {
+    res.redirect('version-0-1/create-account/uk-org-account/check-answers')
+  }
+})
+
+
+// whether user wants to partake in research
+router.post('/overseas-org-research', function (req, res) {
+
+  let choice = req.session.data['overseas-org-research']
+
+  if (choice === 'yes') {
+    res.redirect('version-0-1/create-account/overseas-org-account/yes-research')
+  } else {
+    res.redirect('version-0-1/create-account/overseas-org-account/check-answers')
+  }
+})
+
+
+// which uk organisation type is chosen
+router.post('/org-type', function (req, res) {
+
+  let choice = req.session.data['org-type']
+
+  if (choice === 'company') {
+    res.redirect('version-0-1/create-account/uk-org-account/company-details')
+  } else if (choice == 'charity') {
+    res.redirect('version-0-1/create-account/uk-org-account/charity-details')
+  } else if (choice == 'gov') {
+    res.redirect('version-0-1/create-account/uk-org-account/gov-details')
+  } else if (choice == 'la') {
+    res.redirect('version-0-1/create-account/uk-org-account/la-details')
+  }
 })
 
 
